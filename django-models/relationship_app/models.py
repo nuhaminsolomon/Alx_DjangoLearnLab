0a26fcf418dcee6e5e59d40ce879ab714fef7f66
nuhaminsolomon <nuhamin.sele@gmail.com>
2024-08-18 22:02:15 +0300

    Update models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -22,4 +22,24 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
 
-    
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import   
+ receiver
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ROLE_CHOICES   
+ = (
+        ('admin', 'Admin'),
+        ('librarian', 'Librarian'),
+        ('member', 'Member'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='member')
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
