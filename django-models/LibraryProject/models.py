1b88419703e8ecf29619d08bb5c33755037001c0
Nuhamin <nuhamin.sele@gmail.com>
2024-08-18 17:40:20 +0300

    m

---
+++ django-models/LibraryProject/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name=models.CharField(max_length=100)
+class Book(models.Model):
+    title=models.CharField(max_length=100)
+    author= models.ForeignKey(Author,related_name='books',on_delete=models.CASCADE)
+class Library(models.Model):
+    name=models.CharField(max_length=100)
+    books=models.ManyToManyField(Book,related_name='libraries')
+class Librarian(models.Model):
+    name=models.CharField(max_length=100)
+    library=models.OneToOneField(Library,related_name='librarian',on_delete=models.CASCADE)
+
+    
