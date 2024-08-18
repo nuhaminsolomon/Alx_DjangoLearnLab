121442b9d681c9e03bca8c020aad218dde167c2e
nuhaminsolomon <nuhamin.sele@gmail.com>
2024-08-18 22:07:24 +0300

    Update models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -43,3 +43,21 @@ class UserProfile(models.Model):
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+
+
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+
+class Book(models.Model, PermissionsMixin):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    class Meta:
+        permissions = [
+            ('can_add_book', 'Can add book'),
+            ('can_change_book', 'Can change book'),
+            ('can_delete_book', 'Can delete book'),
+        ]
+
+    def __str__(self):
+        return self.title
