aa6e3b0e45a4d515d4d999464dad602a77da5204
Nuhamin <nuhamin.sele@gmail.com>
2024-08-24 23:03:15 +0300

    permission assigned

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import permission_required
 User = get_user_model()
+from .models import Book
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
 def user_detail(request, pk):
     user = User.objects.get(pk=pk)
