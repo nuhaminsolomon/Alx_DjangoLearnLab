47e45e214385a6d4ae2980f1753014ba5e6fc8bd
Nuhamin <nuhamin.sele@gmail.com>
2024-08-31 22:43:39 +0300

    m

--- api_project/api_project/models.py
+++ api_project/api_project/models.py
@@ -1,8 +1,10 @@
+# api/models.py
+
 from django.db import models
 
 class Book(models.Model):
-    title = models.CharField(max_length=255)  
-    author = models.CharField(max_length=255)  
+    title = models.CharField(max_length=255)  # A field to store the title of the book
+    author = models.CharField(max_length=255)  # A field to store the author's name
 
     def str(self):
-        return f"{self.title} by {self.author}"  
+        return f"{self.title} by {self.author}"  # String representation of the Book model
