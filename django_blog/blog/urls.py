b290607134642fc5b56c141aa11eb17e543eccd8
Nuhamin <nuhamin.sele@gmail.com>
2024-09-14 13:10:36 +0300

    update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,3 +3,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
 ]
+urlpatterns = [
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,55 @@
 from django.shortcuts import render
 
 # Create your views here.
-# blog/views.py
 from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
+            if user:
+                login(request, user)
+                return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def profile(request):
+    return render(request, 'registration/profile.html')
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm(instance=request.user)
+    return render(request, 'registration/edit_profile.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profile')
+        else:
+            return render(request, 'registration/login.html', {'error': 'Invalid credentials'})
+    return render(request, 'registration/login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
 
