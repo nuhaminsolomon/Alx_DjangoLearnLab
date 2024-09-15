e24c9897773096f7269427e4bc52907d544f10ed
nuhaminsolomon <nuhamin.sele@gmail.com>
2024-09-15 15:32:44 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,4 +9,20 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
+    
+    path('posts/', views.PostListView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+
+    path('post/<int:post_id>/comments/new/', add_comment, name='add-comment'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='update-comment'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),
+    path('post/<int:pk>/comments/new/'),
+
+    path('search/', search_posts, name='search'),
+    path('tags/<str:tag_name>/', TaggedPostListView.as_view(), name='tag_posts'),
+
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts-by-tag'),
 ]
