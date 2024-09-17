b2b88fd12ebdd3512f73fb5b38887897af1d1884
Nuhamin <nuhamin.sele@gmail.com>
2024-09-17 20:03:38 +0300

    update

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,6 +3,10 @@ from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
 User = get_user_model()
+from rest_framework import serializers
+
+class MySerializer(serializers.Serializer):
+    my_char_field = serializers.CharField(max_length=100)
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,14 +15,14 @@ class UserSerializer(serializers.ModelSerializer):
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ('username', 'email', 'password', 'bio', 'profile_picture')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
-
+    
 class TokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Token
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -8,6 +8,12 @@ from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from .serializers import RegisterSerializer, UserSerializer, TokenSerializer
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from .serializers import RegisterSerializer
+
+user = get_user_model().objects.get(username='myusername')
+token, created = Token.objects.get_or_create(user=user)
 
 User = get_user_model()
 
@@ -33,4 +39,26 @@ class UserDetailView(APIView):
         user = request.user
         serializer = UserSerializer(user)
         return Response(serializer.data)
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            # Create user with hashed password
+            user = get_user_model().objects.create_user(
+                username=serializer.validated_data['username'],
+                email=serializer.validated_data['email'],
+                password=serializer.validated_data['password']
+            )
+            return Response({
+                'username': user.username,
+                'email': user.email
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+
 
